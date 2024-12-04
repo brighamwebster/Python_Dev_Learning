e15fc66e90e1367b41946c715c3af6300e881a56
brighamwebster <brigham.webster@hoj.net>
2024-12-04 12:24:52 -0700

    push

---
+++ PythonBasics/Basics.py
@@ -0,0 +1,13 @@
+##########01234567890123456
+parrot = "blue wing parrot"
+#Mini Challenge
+
+for i in parrot:
+    print(i)
+
+print(parrot[5])
+print(parrot[3])
+print(parrot[4])
+print(parrot[5])
+print(parrot[6])
+print(parrot[7])
